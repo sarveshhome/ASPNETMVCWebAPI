d8cfa532f09a9492a021020904014a797a2ba645
sarveshhome <sarvesh.carlton@gmail.com>
2019-12-31 12:05:46 +0000

    MVCWebAPI

---
+++ ASPNETMVCWebAPI/ASPNETMVCWeb API/ASPNETMVCWeb API/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using ASPNETMVCWeb_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNETMVCWeb_API.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public  ActionResult Index()
+        {
+            IEnumerable<Product> Product = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost/ASPNETMVCWebAPI/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("products");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Product>>();
+                    readTask.Wait();
+
+                    Product = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    Product = Enumerable.Empty<Product>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(Product);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ASPNETMVCWebAPI/ASPNETMVCWeb API/ASPNETMVCWeb API/Controllers/api/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ASPNETMVCWeb_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNETMVCWeb_API.Controllers.api
+{
+    public class ProductsController : ApiController
+    {
+        List<Product> products = new List<Product> { 
+       new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+       };
+
+        public List<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
